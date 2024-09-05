3cc780673fbee061d10a347dceda78cf7543b475
unknown <uiu.afser@gmail.com>
2024-09-05 15:20:00 +0600

    first commit

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from typing import reveal_type
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+
+def index(request):
+    products =  Product.objects.all()
+    return render(request, 'index.html',{'products':products})
+
+def new(request):
+    return HttpResponse("New Page")
+
